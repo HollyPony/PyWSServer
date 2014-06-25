ac2613f31942e8b9130f0e666b7a87a3f5fbc1d7
Liomka <tankizer@ymail.com>
2014-06-26 00:08:22 +0200

    Dear Python, I've fix your indentation for my pleasure.

--- hello.py
+++ hello.py
@@ -2,20 +2,17 @@ import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
- 
-class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello world")
- 
-	def main():
-		application = tornado.web.Application([
-			(r"/", MainHandler),
-		])
 
-	http_server = tornado.httpserver.HTTPServer(application)
-	port = int(os.environ.get("PORT", 5000))
-	http_server.listen(port)
-	tornado.ioloop.IOLoop.instance().start()
- 
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+       self.write("Hello world")
+
+def main():
+    application = tornado.web.Application([(r"/", MainHandler),])
+    http_server = tornado.httpserver.HTTPServer(application)
+    port = int(os.environ.get("PORT", 5000))
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
+
 if __name__ == "__main__":
-	main()
+    main()
