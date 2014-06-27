6f3ea1a297b67fd17bd92ecb64f310bf9979428a
Liomka <tankizer@ymail.com>
2014-06-27 09:09:34 +0200

    Delete useless code

--- index.py
+++ index.py
@@ -40,8 +40,6 @@ class ApiHandler(cyclone.web.RequestHandler):
         value = self.get_argument("value")
         data = {"id": id, "value": value}
         data = json.dumps(data)
-        for c in clients:
-            c.write_message(data)
 
     @cyclone.web.asynchronous
     def post(self):
